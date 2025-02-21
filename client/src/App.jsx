d2f58b78272128bcb20279ab1000dab7c88cef62
abhi1580 <abhayrpanchal@gmail.com>
2025-02-21 22:57:09 +0530

    paypal-cancel page added and also routing for it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -101,7 +101,7 @@ function App() {
           <Route path="listing" element={<ShoppingList />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="account" element={<ShoppingAccount />} />
-          <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="paypal-cancel" element={<PaypalReturnPage />} />
           <Route path="payment-success" element={<PaymentSuccessPage />} />
           <Route path="search" element={<SearchProducts />} />
         </Route>
