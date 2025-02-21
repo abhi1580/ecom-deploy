f3d6e1d909bbaa48dfbc517a70d7237e5d0129bc
abhi1580 <abhayrpanchal@gmail.com>
2025-02-21 21:09:14 +0530

    resetTokenAndCredentials imported in admin header

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "../ui/button";
 import { LogOut, AlignJustify } from "lucide-react";
 import { useDispatch } from "react-redux";
-import { logoutUser } from "@/store/auth-slice";
+import { logoutUser, resetTokenAndCredentials } from "@/store/auth-slice";
 import { useNavigate } from "react-router-dom";
 
 const AdminHeader = ({ setOpen }) => {
