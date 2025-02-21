cc550d01ef0c446fbc2890dd5c4075ede9cfe199
abhi1580 <abhayrpanchal@gmail.com>
2025-02-21 16:30:08 +0530

    If the cart items is 0 then checkout button from cart will not appear

--- client/src/components/shopping-view/cart-wrapper.jsx
+++ client/src/components/shopping-view/cart-wrapper.jsx
@@ -18,35 +18,41 @@ const UserCartWrapper = ({ cartItems, setOpenCartSheet }) => {
           0
         )
       : 0;
+
   return (
     <SheetContent className="sm:max-w-md">
       <SheetHeader>
         <SheetTitle>Your Cart</SheetTitle>
       </SheetHeader>
       <div className="mt-8 space-y-4">
-        {cartItems && cartItems.length > 0
-          ? cartItems.map((item) => (
-              <UserCartItemsContent key={item.productId} cartItem={item} />
-            ))
-          : "Your cart is empty"}
+        {cartItems && cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <UserCartItemsContent key={item.productId} cartItem={item} />
+          ))
+        ) : (
+          <p>Your cart is empty</p>
+        )}
       </div>
-      <div className="mt-8 space-y-4">
-        <div className="flex justify-between">
-          <span className="font-bold">Total</span>
-          <span className="font-bold">₹{totalCartAmount.toFixed(2)}</span>
-        </div>
-      </div>
-      <Button
-        onClick={() => {
-          // if (cartItems.length > 0) {
-          navigate("/shop/checkout");
-          setOpenCartSheet(false);
-          // }
-        }}
-        className="w-full mt-6"
-      >
-        Checkout
-      </Button>
+
+      {cartItems && cartItems.length > 0 && (
+        <>
+          <div className="mt-8 space-y-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Total</span>
+              <span className="font-bold">₹{totalCartAmount.toFixed(2)}</span>
+            </div>
+          </div>
+          <Button
+            className="w-full mt-6"
+            onClick={() => {
+              navigate("/shop/checkout");
+              setOpenCartSheet(false);
+            }}
+          >
+            Checkout
+          </Button>
+        </>
+      )}
     </SheetContent>
   );
 };
